1b741f9f38169c8761627b4b7e63cfac267e1ada
SergeyBogomolovv <bogomolovs693@gmail.com>
2024-01-01 14:10:12 +0300

    changed branches

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = setupStore()
 root.render(
-    <BrowserRouter >
+    <BrowserRouter basename='/Mikhail-Milutin2'>
         <Provider store={store}>
             <App />
         </Provider>
