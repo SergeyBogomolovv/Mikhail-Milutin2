1cab6358ddc1e7ba763965176d0ac37961dacbc7
SergeyBogomolovv <bogomolovs693@gmail.com>
2024-01-02 11:34:44 +0300

    jfgdj

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
-import './App.css';
-import NewYear from './components/newYear/newYear';
-import About from './components/aboutpage/about';
-import Contacts from './components/contacts/contacts';
-import Main from './components/mainpage/main';
-import Politica from './components/politica/politica';
-import Interesting from './components/interesting/interesting';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Layout from './components/Layout';
-import Featured from './components/featured/Featured';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import { AnimatePresence } from 'framer-motion';
+import './App.css'
+import NewYear from './components/newYear/newYear'
+import About from './components/aboutpage/about'
+import Contacts from './components/contacts/contacts'
+import Main from './components/mainpage/main'
+import Politica from './components/politica/politica'
+import Interesting from './components/interesting/interesting'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Layout from './components/Layout'
+import Featured from './components/featured/Featured'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import { AnimatePresence } from 'framer-motion'
 
 function App() {
   const location = useLocation()
   return (
-   <> 
-    {/* <AnimatePresence>
-    <Routes location={location} key={location.pathname}>
-          <Route path='/' element={ <Layout/>}>
-          <Route index element={<Main/>}/>
-          <Route path='featured' element={<Featured/>}/>
-          <Route path='about' element={<About/>}/>
-          <Route path='main' element={<Main/>}/>
-          <Route path='NewYear' element={<NewYear/>}/>
-          <Route path='Interesting' element={<Interesting/>}/>
-          <Route path='Politica' element={<Politica/>}/>
-        </Route>   
-      </Routes>
-    </AnimatePresence>
-    <Contacts/> */}
-      
-      
-      <Header/>
+    <>
+      <Header />
       <AnimatePresence>
-      <Routes>
-          <Route path='/' element={<Main/>}/>
-          <Route path='/featured' element={<Featured/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/main' element={<Main/>}/>
-          <Route path='/NewYear' element={<NewYear/>}/>
-          <Route path='/Interesting' element={<Interesting/>}/>
-          <Route path='/Politica' element={<Politica/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/featured' element={<Featured />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/NewYear' element={<NewYear />} />
+          <Route path='/Interesting' element={<Interesting />} />
+          <Route path='/Politica' element={<Politica />} />
+        </Routes>
       </AnimatePresence>
-      <Contacts/>
-      <Footer/>
-  </>
-  );
+      <Contacts />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
